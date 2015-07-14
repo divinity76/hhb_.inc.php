9a9cad5cce0a6494350918fe7fb7ea61b82fd4cd
divinity76 <divinity76@gmail.com>
2015-07-14 14:46:30 +0200

    hhb_curl_exec2 : with $headers and $cookies !
    i really hope there's a better way to grab cookies :( this looks butt-ugly.. never the less, its tested seems to work flawlessly

--- hhb_.inc.php
+++ hhb_.inc.php
@@ -89,7 +89,66 @@ function hhb_curl_exec($ch, $url) {
 }
 
 
-
+	function hhb_curl_exec2($ch,$url,&$returnHeaders=array(),&$returnCookies=array()){
+		$returnHeaders=array();
+		$returnCookies=array();
+    	if(!is_resource($ch) || get_resource_type($ch)!=='curl')
+		{
+			throw new InvalidArgumentException('$ch must be a curl handle!');
+		}
+		if(!is_string($url))
+		{
+			throw new InvalidArgumentException('$url must be a string!');
+		}
+		curl_setopt($ch, CURLOPT_HEADER, 1);
+		$html=hhb_curl_exec($ch,$url);
+		$headers=array();
+		$crlf="\x0d\x0a";
+		$thepos=strpos($html,$crlf.$crlf,0);
+		$headersString=substr($html,0,$thepos);
+		$headerArr=explode($crlf,$headersString);
+		$returnHeaders=$headerArr;
+		unset($headersString,$headerArr);
+		$htmlBody=substr($html,$thepos+4);//should work on utf8/ascii headers... utf32? not so sure..
+		unset($html);
+		//I REALLY HOPE THERE EXIST A BETTER WAY TO GET COOKIES.. good grief this looks ugly..
+		//at least it's tested and seems to work perfectly...
+		$grabCookieName=function($str){
+			$ret="";
+			$i=0;
+			for($i=0;$i<strlen($str);++$i){
+				if($str[$i]===' '){continue;}
+				if($str[$i]==='='){break;}
+				$ret.=$str[$i];
+			}
+			return urldecode($ret);
+		};
+		foreach($returnHeaders as $header){
+			//Set-Cookie: crlfcoookielol=crlf+is%0D%0A+and+newline+is+%0D%0A+and+semicolon+is%3B+and+not+sure+what+else
+			/*Set-Cookie:ci_spill=a%3A4%3A%7Bs%3A10%3A%22session_id%22%3Bs%3A32%3A%22305d3d67b8016ca9661c3b032d4319df%22%3Bs%3A10%3A%22ip_address%22%3Bs%3A14%3A%2285.164.158.128%22%3Bs%3A10%3A%22user_agent%22%3Bs%3A109%3A%22Mozilla%2F5.0+%28Windows+NT+6.1%3B+WOW64%29+AppleWebKit%2F537.36+%28KHTML%2C+like+Gecko%29+Chrome%2F43.0.2357.132+Safari%2F537.36%22%3Bs%3A13%3A%22last_activity%22%3Bi%3A1436874639%3B%7Dcab1dd09f4eca466660e8a767856d013; expires=Tue, 14-Jul-2015 13:50:39 GMT; path=/
+				Set-Cookie: sessionToken=abc123; Expires=Wed, 09 Jun 2021 10:18:14 GMT;
+				//Cookie names cannot contain any of the following '=,; \t\r\n\013\014'
+				//
+			*/
+			if(stripos($header,"Set-Cookie:")!==0){continue;/**/}
+			$header=trim(substr($header,strlen("Set-Cookie:")));
+			while(strlen($header)>0){
+				$cookiename=$grabCookieName($header);
+				$returnCookies[$cookiename]='';
+				$header=substr($header,strlen($cookiename)+1);//also remove the = 
+				if(strlen($header)<1){break;};
+				$thepos=strpos($header,';');
+				if($thepos===false){//last cookie in this Set-Cookie.
+					$returnCookies[$cookiename]=urldecode($header);
+					break;
+				}
+				$returnCookies[$cookiename]=urldecode(substr($header,0,$thepos));
+				$header=trim(substr($header,$thepos+1));//also remove the ;
+			}
+		}
+		unset($header,$cookiename,$thepos);
+		return $htmlBody;
+	}
 
 
 function hhb_var_dump() {
