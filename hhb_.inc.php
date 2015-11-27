34e87e2dff9803d295a247b8999fad4f2c702a03
divinity76 <divinity76@gmail.com>
2015-11-27 12:36:38 +0700

    throw RuntimeException if tmpfile() returns false

--- hhb_.inc.php
+++ hhb_.inc.php
@@ -188,6 +188,9 @@ function hhb_curl_exec2($ch, $url, &$returnHeaders = array(), &$returnCookies = 
         throw new InvalidArgumentException('$url must be a string!');
     }
     $verbosefileh = tmpfile();
+    if($verbosefileh===false){
+        throw new RuntimeException('can not create a tmpfile for curl's stderr. tmpfile returned false');
+    }
     $verbosefile  = stream_get_meta_data($verbosefileh);
     $verbosefile  = $verbosefile['uri'];
     curl_setopt($ch, CURLOPT_VERBOSE, 1);
